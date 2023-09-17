874c3c582d449f1b342125d132325980da0d6e0a
KaSaNaa <pasindudeshitha123@gmail.com>
2023-09-17 22:18:32 +0530

    Initial commit

---
+++ src/Message.tsx
@@ -0,0 +1,14 @@
+// PascalCasing : Capitalize first letter of a word
+
+function Message() {
+    // JSX: JavaScript XML
+    const name = '';
+    if(name) {
+        return <h1>Hello {name}</h1>;
+    } else {
+        return <h1>Hello World</h1>;
+    }
+}
+
+export default Message;
+//z
